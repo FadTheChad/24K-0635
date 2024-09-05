5f767ad17dbc16d512e7651974bd150f1900e6fd
Fad F <90547391+FadTheChad@users.noreply.github.com>
2024-09-05 15:30:52 +0500

    Create Q4.c

---
+++ Lab 3/Q4.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+int main()
+{
+  float distance, fuel_price_a, fuel_price_b, fuel_avg;
+
+  printf("Enter distance, fuel)
+
+  // ...INCOMPLETE FOR NOW
+    
+  float number_of_litres = distance/fuel_avg;
+  float total_price_a = fuel_price_a * (number_of_litres/2);
+  float total_price_b = fuel_price_b * (number_of_litres/2);
+
+  
+}
