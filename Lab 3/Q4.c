11b3a2f42c20e8cd135682d4f4c4889b4f3c4801
Fad F <90547391+FadTheChad@users.noreply.github.com>
2024-09-08 17:58:58 +0500

    Update Q4.c

--- Lab 3/Q4.c
+++ Lab 3/Q4.c
@@ -4,13 +4,24 @@ int main()
 {
   float distance, fuel_price_a, fuel_price_b, fuel_avg;
 
-  printf("Enter distance, fuel)
+  printf("Enter distance (one trip), fuel price from a to b, fuel price from b to a, fuel avg:");
+  scanf("%f %f %f %f", &distance, &fuel_price_a, &fuel_price_b, &fuel_avg);
 
-  // ...INCOMPLETE FOR NOW
-    
-  float number_of_litres = distance/fuel_avg;
-  float total_price_a = fuel_price_a * (number_of_litres/2);
-  float total_price_b = fuel_price_b * (number_of_litres/2);
-
+  if (distance < 0 || fuel_price_a < 0 || fuel_price_b < 0 || fuel_avg < 0)
+  {
+    // values toh positive daalo, bhai
+    printf("Values must be positive");
+    return 1;
+  }
   
+  float number_of_litres = distance/fuel_avg; // one trip
+  float total_price_a = fuel_price_a * (number_of_litres);
+  float total_price_b = fuel_price_b * (number_of_litres);
+  
+  float total_price = total_price_a + total_price_b;
+  float total_fuel = 2 * number_of_litres;
+
+  printf("Total Price: %f\nTotal Fuel: %f", total_price, total_fuel);
+
+  return 0;
 }
