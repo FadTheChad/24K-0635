0136fcd36e18bbe2b1a3649d1559a59d158fa9d4
Fad F <90547391+FadTheChad@users.noreply.github.com>
2024-09-17 22:27:03 +0500

    Update Q6.c

--- Lab 4/Q6.c
+++ Lab 4/Q6.c
@@ -3,7 +3,7 @@
 int main()
 {
     int cost;
-    float discount;
+    float discount = 0;
 
     printf("Enter cost: ");
     scanf("%d", &cost);
@@ -11,7 +11,7 @@ int main()
     if (cost >= 6000) discount == 0.35;
     else if (cost >= 4000) discount == 0.20;
     else if (cost >= 2000) discount == 0.10;
-    else discount = 0.05;
+    else if (cost >= 500) discount = 0.05;
 
     cost = cost * (1 - discount);
 
